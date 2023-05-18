c968669b6beeddd8720ffa04f8dc4cbff66ead66
ThunderfuryofWindseeker <nedyalkov1717@gmail.com>
2023-05-18 16:25:57 +0300

    .

--- index.js
+++ index.js
@@ -2,30 +2,42 @@ const TelegramBot = require('node-telegram-bot-api');
 
 
 const token = '6207997807:AAHJW7K8sSzymMiHUnMvuZGG59T-2kKqL78';
-const webAppUrl = 'https://www.youtube.com/watch?v=MzO-0IYkZMU&t=925s';
+const webAppUrl = 'https://7de0-217-156-4-24.ngrok-free.app';
+const form = `/form`
 
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
-    await bot.sendMessage(chatId,'Ниже появится кнопка, заполни форму', {
+    await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
         reply_markup: {
             inline_keyboard: [
-                [{text:'Заполнить форму', web_app:{url:webAppUrl}}]
+                [{ text: 'Заполнить форму', web_app: { url: webAppUrl + form } }]
             ]
         }
-    })
-});
-
-bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
-    await bot.sendMessage(chatId,'Ниже появится кнопка, заполни форму', {
+    });
+    await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
         reply_markup: {
             keyboard: [
-                [{text:'Зайти в интернет магазин', web_app:{url:webAppUrl}}]
+                [{ text: 'Заполнить форму', web_app: { url: webAppUrl + form } }]
             ]
         }
     })
+    if (msg?.web_app_data?.data) {
+        try {
+            const data = JSON.parse(msg?.web_app_data?.data);
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            await bot.sendMessage(chatId, `Your country: ${data?.country}`)
+            await bot.sendMessage(chatId, `Your street: ${data?.street}`)
+
+            setTimeout(async () => {
+                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате')
+            }, 3000)
+        } catch (e) {
+            console.error(e);
+        }
+
+    }
 });
+
